5dd9629ea64fe43bfc46b6bf2b4c5a0f5f98ddb8
cosmodesant49 <emilmaldybaev49@gmail.com>
2024-09-27 20:29:47 +0600

    some fix

--- app/src/main/java/com/dezis/geeks_dezis/presentation/activity/ServiceScreenActivity.kt
+++ app/src/main/java/com/dezis/geeks_dezis/presentation/activity/ServiceScreenActivity.kt
@@ -6,7 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import com.dezis.geeks_dezis.R
 import com.google.android.material.button.MaterialButton
 
-class serviceScreenActivity : AppCompatActivity() {
+class ServiceScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_service_screen)
