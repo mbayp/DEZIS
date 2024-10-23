cc65aca25e61d1289cdee9e249f9adfc162ede5c
Asanaliev Temirlan <tonyadam050607@gmail.com>
2024-10-23 19:17:55 +0600

    Переделал home screen

--- app/src/main/java/com/dezis/geeks_dezis/presentation/fragments/home/InformationModel.kt
+++
@@ -1,8 +0,0 @@
-package com.dezis.geeks_dezis.presentation.fragments.home
-
-import javax.inject.Inject
-
-data class InformationModel @Inject constructor(
-    val id: Int = 0,
-    val description: String
-)
