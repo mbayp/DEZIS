4cb6ecad93c33887e59ed45d23aa4a46ea6ed5f7
cosmodesant49 <emilmaldybaev49@gmail.com>
2024-10-25 19:13:54 +0600

    some fix

--- app/src/main/java/com/dezis/geeks_dezis/core/common/Constants.kt
+++ app/src/main/java/com/dezis/geeks_dezis/core/common/Constants.kt
@@ -1,7 +1,9 @@
 package com.dezis.geeks_dezis.core.common
 
 object Constants {
-    const val BASE_URL = "http://209.38.228.54:8084/api/v1/"
+
+    //const val BASE_URL = "http://209.38.228.54:8084/api/v1/"
+    const val BASE_URL = "https://dezis.pp.ua/api/v1/"
     const val NETWORK_TIMEOUT = 60L
     const val get_booking = "contact/booking/"
     const val post_booking = "contact/booking/"
