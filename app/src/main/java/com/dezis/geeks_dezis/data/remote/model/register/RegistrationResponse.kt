dbe9f08c88c6b45a80b7e80a5be81fb27330350b
cosmodesant49 <emilmaldybaev49@gmail.com>
2024-11-08 18:26:48 +0600

    some fix

--- app/src/main/java/com/dezis/geeks_dezis/data/remote/model/register/RegistrationResponse.kt
+++ app/src/main/java/com/dezis/geeks_dezis/data/remote/model/register/RegistrationResponse.kt
@@ -6,10 +6,6 @@ data class RegistrationResponse(
     val email: String,
     val apartmentNumber: String,
     val address: String,
-<<<<<<< HEAD:app/src/main/java/com/dezis/geeks_dezis/data/remote/model/RegistrationResponse.kt
-    val number:String
-)
-=======
+    val number:String,
     val otp: String
 )
->>>>>>> 2384ff11e8e03d7e2dec91b9755cbc0f3a689273:app/src/main/java/com/dezis/geeks_dezis/data/remote/model/register/RegistrationResponse.kt
